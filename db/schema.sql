d4f4096a1539c69d86625d189e590be3146725af
Hongjing <hongjingdeng@gmail.com>
2020-09-09 20:05:29 -0400

    updating invoice table as needed

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,31 @@
-DROP DATABASE clientsDB;
+DROP DATABASE IF Exists clientsDB;
 CREATE DATABASE clientsDB;
 
 USE clientsDB;
 
--- create table burgers -- 
+-- create table clients-- 
 CREATE TABLE clients (
     id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(80) NOT NULL,
+    clientName VARCHAR(80) NOT NULL,
     address VARCHAR(30) NOT NULL,
     email VARCHAR(80) NOT NULL,
     phone INT(12) NOT NULL,
     newClients BOOLEAN DEFAULT true,
-    serviceID INT Foreign Key,
-    PRIMARY KEY(id)
+    invoiceID INT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (invoiceID) REFERENCES invoice(id) ON DELETE CASCADE
 );
 
-CREATE TABLE service (
-    id AUTO_INCREMENT NOT NULL,
+-- create table invoice-- 
+CREATE TABLE invoice (
+    id INT AUTO_INCREMENT NOT NULL,
     clientName VARCHAR(30) NOT NULL,
-    clientAddress , --how to pull from TABLE clients address--
-    finishedJob VARCHAR(500) NOT NULL,
+    jobAddress VARCHAR(30) NOT NULL, -- how to pull from TABLE clients address? --
+    jobDescription VARCHAR(500) NOT NULL,
+    jobStartDate VARCHAR(10) NOT NULL,
+    jobFinishDate VARCHAR(10) NOT NULL,
     invoicedAmount DECIMAL(8,2) NULL,
+    invoiceDue INT NULL,
     paid BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 )
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE clientsDB;
+
+INSERT INTO clients ()
